bf157ab469732c35054f5853e9e5cbbc8d2a1e3e
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-08-18 17:16:39 +0200

    chore: remove tsup

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,20 +1,4 @@
 import { defineConfig } from "astro/config";
-// import path from "node:path";
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    // optimizeDeps: {
-    //   link: ["astro-capo"],
-    // },
-    // alias: {
-    //   "astro-capo": "/@linked/astro-capo/index.ts",
-    //   "/@linked/astro-capo/": path.resolve(
-    //     require.resolve("astro-capo/package.json"),
-    //     "../src"
-    //   ),
-    // },
-  },
-});
+export default defineConfig({});
